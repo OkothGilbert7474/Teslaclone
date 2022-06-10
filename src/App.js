99f68617ad85977194a7c0f48cd05ceb67c40cc9
OkothGilbert7474 <100310659+OkothGilbert7474@users.noreply.github.com>
2022-06-10 17:01:30 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+//import {Counter} from './features/Counter/Counter';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+   <Header/> 
+   <Home/>
+   
+    </div>
+  );
+}
+
+export default App;
+
